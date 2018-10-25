776156695bbda31f15f1fb386673eaf30669621b
ejclover <smithearl37@gmail.com>
2018-10-25 15:29:27 -0400

    Removed all the images and started writing the descriptions

---
+++ speech.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function () {
+    
+    $(".mybtn").mouseenter(function(){
+        $(this).css("background-color","#FF5A09");
+        $(this).css("bordor-color","#FF5A09");
+    
+    
+    
+  });
+    $(".mybtn").mouseleave(function(){
+        $(this).css("background-color","#be4f0c");
+        $(this).css("border-color","#be4f0c");
+        
+        
+        
+    })
+
+
+
+});
